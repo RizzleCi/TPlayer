59a3e2320fd04fdb5d569f28c23009b4fa1ac1d2
Rizzle <734783679@qq.com>
2015-11-12 22:39:59 +0800

    update some css

--- js/index.js
+++ js/index.js
@@ -24,7 +24,8 @@ var timeControl = function (player,event) {
 	var fulltime = document.getElementById('full');
 	var timeline = document.getElementById('timeline');
 	var timediv = timeline.getElementsByTagName('div')[0];
-
+	var dot = timediv.getElementsByTagName('span')[0];
+	
 	fulltime.innerHTML = Math.ceil(player.duration);
 	setInterval(function () {
 		nowtime.innerHTML = Math.ceil(player.currentTime);
@@ -35,6 +36,7 @@ var voiceControl = function (player,event) {
 	var voice = document.getElementById('voice');
 	var sounddiv = document.getElementById('sound');
 	var insidediv = sounddiv.getElementsByTagName('div')[0];
+	var dot = insidediv.getElementsByTagName('span')[0];
 	insidediv.style.width = sounddiv.style.width;
 	var sound = parseInt(insidediv.style.width)/parseInt(sounddiv.style.width);
 	console.log(sound)
@@ -47,7 +49,7 @@ var voiceControl = function (player,event) {
 		};
 	}
 
-	sounddiv.onmouseup = function (event) {
+	sounddiv.onclick = function (event) {
 		sound = (event.clientX-sounddiv.offsetLeft)/parseInt(sounddiv.style.width);
 		player.volume = sound;
 		insidediv.style.width = sound * parseInt(sounddiv.style.width) + "px";
