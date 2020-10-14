921c5dc577a22e5a8321c5e4a96b1cd255dc1782
chengxinyu <chengxinyu01@qiyi.com>
2020-10-14 19:33:59 +0800

    lrcs

--- js/index.js
+++ js/index.js
@@ -27,6 +27,44 @@ const lycs = {
     // [x-trans]哈和热派一起
     // [00:16.132]みんなでぬくぬく
     // [x-trans]大家都暖烘烘
+  ],
+  2: [
+    [0.68, '想要传送一封简讯给你'],
+    [4.82, '我好想好想你'],
+    [7.36, '想要立刻打通电话给你'],
+    [9.79, '我好想好想你'],
+    [12.28, '每天起床的第一件事情'],
+    [14.72, '就是好想好想你'],
+    [17.5, '无论晴天还是下雨'],
+    [19.84, '都好想好想你']
+  ],
+  3: [
+    [0.3, 'だって可愛く変わりたいんだ'],
+    [5.01, '知らない顔しても意識し合ってる'],
+    [9.01, '背伸びしたメイクドキドキだけど'],
+    [13.31, '気付いてくれなきゃダメなんだから']
+  ],
+  5: [
+    [0.965, '你问我爱你有多深'],
+    [6.685, '我爱你有几分'],
+    [12.554, '你去想一想'],
+    [15.862, '你去看一看'],
+    [18.930, '月亮代表我的心'],
+    [24.947, '你去想一想'],
+    [27.939, '你去看一看'],
+    [30.701, '月亮代表我的心']
+  ],
+  6: [
+    [0, '是你让我看见干枯沙漠开出花一朵'],
+    [4.969, '是你让我想要每天为你写一首情歌'],
+    [9.7, '用最浪漫的副歌'],
+    [11.864, '你也轻轻的附和'],
+    [14.559, '眼神坚定着我们的选择'],
+    [18.926, '是你让我的世界从那刻变成粉红色'],
+    [23.431, '是你让我的生活从此都只要你配合'],
+    [27.845, '爱要精心来雕刻'],
+    [30.233, '我是米开朗基罗'],
+    [32.732, '用心刻画最幸福的风格']
   ]
 }
 var createPlayer = (function (window) {
@@ -43,31 +81,11 @@ var createPlayer = (function (window) {
     controls = argument;
     myPlayer.render()
   };
-  //转换时间格式的函数
-  var secToMin = function (sec) {
-    if (typeof sec === "number" && sec === sec) {
-      var min = Math.floor(sec/60);
-      var newsec = sec%60;
-      if (min<10) {
-        var realmin = "0" + min.toString();
-      }else{
-        var realmin = min.toString();
-      };
-      if (newsec<10) {
-        var realsec = "0" + newsec.toString();
-      }else{
-        var realsec = newsec.toString();
-      };
-      var time = realmin+":"+realsec;
-      return time
-    }else{return "00:00"};  
-  }
 
   var rollLrc = function (lrc) {
     var lrcdiv = document.getElementById('lrc');
     var lrcul = lrcdiv.getElementsByTagName('ul')[0];
     lrcul.innerHTML = "";
-    //console.log(lrc);
     const listItems = lrc.map(item => {
       const li = document.createElement("li");
       li.innerHTML = item[1];
@@ -80,16 +98,12 @@ var createPlayer = (function (window) {
     // lrcul.style.top = "200px"
     myPlayer.player.ontimeupdate = function () {
       var time = myPlayer.player.currentTime.toFixed(3);
-
-
       for (var i = 0; i < lrc.length; i++) {
-        var newtext = listItems[i + 1];
         var now = listItems[i]; 
         if (lrc[i][0] <= time && (lrc[i + 1] ? lrc[i + 1][0] > time : true)) {
           now.className = 'active';
         } else {
           now.className = '';
-
         }; 
       };
     }
